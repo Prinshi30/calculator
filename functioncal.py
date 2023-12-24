4da1a21c0c131c979445ba223c15ff93acd282df
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2023-12-24 15:55:16 +0530

    functioncal.py

--- functioncal.py
+++ functioncal.py
@@ -14,19 +14,24 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+# This function reminde two numbers
+def module(x, y):
+    return x % y
+
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.modulo")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -45,6 +50,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+         
+        elif choice == '5':
+            print(num1, "%", num2, "=", module(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
